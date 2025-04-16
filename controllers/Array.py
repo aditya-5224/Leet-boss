c1351ae8edbe596d2215c65e855052c24fb9499a
aditya-5224 <yadi.tya5224@gmail.com>
2025-04-16 15:42:31 +0530

    array question 3

--- controllers/Array.py
+++ controllers/Array.py
@@ -1,3 +1,21 @@
+# Given two sorted arrays nums1 and nums2 of size m and n respectively, return the median of the two sorted arrays.
+
+# The overall run time complexity should be O(log (m+n)).
+
+ 
+
+# Example 1:
+
+# Input: nums1 = [1,3], nums2 = [2]
+# Output: 2.00000
+# Explanation: merged array = [1,2,3] and median is 2.
+# Example 2:
+
+# Input: nums1 = [1,2], nums2 = [3,4]
+# Output: 2.50000
+# Explanation: merged array = [1,2,3,4] and median is (2 + 3) / 2 = 2.5.
+
+
 class Solution(object):
     def findMedianSortedArrays(self, nums1, nums2):
         nums1+=nums2
@@ -8,3 +26,13 @@ class Solution(object):
            
         else:
             return nums1[lnth//2]
+        
+
+
+
+# Given an integer x, return true if x is a palindrome, and false otherwise.
+
+
+class Solution(object):
+    def isPalindrome(self, x):
+        return True if str(x)==str(x)[::-1] else False
