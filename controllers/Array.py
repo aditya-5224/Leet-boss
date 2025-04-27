dc4f2220a313f53744dee49601bd651bf382fe9c
aditya-5224 <yadi.tya5224@gmail.com>
2025-04-27 13:18:50 +0530

    array question 8

--- controllers/Array.py
+++ controllers/Array.py
@@ -108,3 +108,21 @@ class Solution(object):
                 if i[j]<0:
                     cnt+=1
         return cnt
+    
+# Given a 0-indexed integer array nums of size n, find the maximum difference between nums[i] and nums[j] (i.e., nums[j] - nums[i]), such that 0 <= i < j < n and nums[i] < nums[j].
+# Return the maximum difference. If no such i and j exists, return -1.
+
+
+class Solution(object):
+    def maximumDifference(self, nums):
+        l=[]
+        for i in range(len(nums)-1):
+            for j in range(i+1,len(nums)):
+                if nums[j] - nums[i]>0:
+                    l+=[nums[j] - nums[i]]
+                else:
+                    l+=[nums[j] - nums[i]]
+        if max(l)>0:
+            return (max(l))
+        else:
+            return (-1)
