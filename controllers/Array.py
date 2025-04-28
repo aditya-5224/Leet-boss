d6282d602138f773cf2d8633ce8a00a0c578f7f5
aditya-5224 <yadi.tya5224@gmail.com>
2025-04-28 13:36:31 +0530

    array question 9

--- controllers/Array.py
+++ controllers/Array.py
@@ -126,3 +126,15 @@ class Solution(object):
             return (max(l))
         else:
             return (-1)
+
+
+# Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+
+class Solution(object):
+    def searchInsert(self, nums, target):
+        if target in nums:
+            return nums.index(target)
+        else:
+            nums+=[target]
+            nums.sort()
+            return nums.index(target)
