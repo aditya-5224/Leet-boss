eeb992b65376e87d0578bd715b96c462d813423c
aditya-5224 <yadi.tya5224@gmail.com>
2025-04-29 13:27:05 +0530

    array question 10

--- controllers/Array.py
+++ controllers/Array.py
@@ -138,3 +138,13 @@ class Solution(object):
             nums+=[target]
             nums.sort()
             return nums.index(target)
+
+
+# Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+# You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+class Solution(object):
+    def singleNumber(self, nums):
+        for i in nums:
+            if nums.count(i)==1:
+                return i
