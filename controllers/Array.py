3d99fa2c88244701ad0cd28e866440b698b7ec26
aditya-5224 <yadi.tya5224@gmail.com>
2025-05-07 13:53:19 +0530

    array question 17

--- controllers/Array.py
+++ controllers/Array.py
@@ -230,3 +230,13 @@ class Solution(object):
             return [min(lst),max(lst)]
         else:
             return [-1,-1]
+        
+
+# Given an integer array nums where every element appears three times except for one, which appears exactly once. Find the single element and return it.
+# You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+class Solution(object):
+    def singleNumber(self, nums):
+        for i in nums:
+            if nums.count(i)==1:
+                return i
