76cb3e561cd1308641c74e73852b2e984b8133e8
aditya-5224 <yadi.tya5224@gmail.com>
2025-05-08 14:10:29 +0530

    string question 4

--- controllers/String.py
+++ controllers/String.py
@@ -25,3 +25,16 @@ class Solution(object):
     def lengthOfLastWord(self, s):
         lst=s.split()
         return len(lst[-1])
+    
+
+# Given an input string s, reverse the order of the words.
+# A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+# Return a string of the words in reverse order concatenated by a single space.
+# Note that s may contain leading or trailing spaces or multiple spaces between two words. The returned string should only have a single space separating the words. Do not include any extra spaces.
+
+
+class Solution(object):
+    def reverseWords(self, s):
+        lst=s.split()
+        st=" ".join(lst[::-1])
+        return (st)
