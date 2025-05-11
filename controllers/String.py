dff3c54aeabcd437c0e01006ba6e3b6f8f3d4c64
aditya-5224 <yadi.tya5224@gmail.com>
2025-05-11 10:32:54 +0530

    string question 5

--- controllers/String.py
+++ controllers/String.py
@@ -38,3 +38,15 @@ class Solution(object):
         lst=s.split()
         st=" ".join(lst[::-1])
         return (st)
+    
+# Write a function that reverses a string. The input string is given as an array of characters s.
+# You must do this by modifying the input array in-place with O(1) extra memory.
+
+class Solution(object):
+    def reverseString(self, s):
+        left=0
+        right=len(s)-1
+        while left<right:
+            s[left],s[right]=s[right],s[left]
+            left+=1
+            right-=1
