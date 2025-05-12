858b62c8a820009356cf8eb5c10b362f9c8e1775
aditya-5224 <yadi.tya5224@gmail.com>
2025-05-12 13:18:23 +0530

    array question 19

--- controllers/Array.py
+++ controllers/Array.py
@@ -249,3 +249,13 @@ class Solution(object):
         for i in range(len(nums)+1):
             if i not in nums:
                 return i
+            
+# Given a zero-based permutation nums (0-indexed), build an array ans of the same length where ans[i] = nums[nums[i]] for each 0 <= i < nums.length and return it.
+# A zero-based permutation nums is an array of distinct integers from 0 to nums.length - 1 (inclusive).
+
+class Solution(object):
+    def buildArray(self, nums):
+        ans=[]
+        for i in range(len(nums)):
+            ans+=[nums[nums[i]]]
+        return ans
