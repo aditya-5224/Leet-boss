a115d5f1c00483b84ac19bb423e56947f5462098
aditya-5224 <yadi.tya5224@gmail.com>
2025-05-15 13:37:19 +0530

    string question 6

--- controllers/String.py
+++ controllers/String.py
@@ -50,3 +50,14 @@ class Solution(object):
             s[left],s[right]=s[right],s[left]
             left+=1
             right-=1
+
+
+# We define the usage of capitals in a word to be right when one of the following cases holds:
+# All letters in this word are capitals, like "USA".
+# All letters in this word are not capitals, like "leetcode".
+# Only the first letter in this word is capital, like "Google".
+# Given a string word, return true if the usage of capitals in it is right.
+
+class Solution(object):
+    def detectCapitalUse(self, word):
+        return True if word.isupper() or word.islower() or word.capitalize()==word else False
