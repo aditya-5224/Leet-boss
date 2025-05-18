91de2c87f69174650e6f444c6605da39689d6db3
aditya-5224 <yadi.tya5224@gmail.com>
2025-05-18 14:25:31 +0530

    string question 8

--- controllers/String.py
+++ controllers/String.py
@@ -73,3 +73,15 @@ class Solution(object):
             else:
                 dct+=i
         return dct
+    
+
+# You are given a string s. The score of a string is defined as the sum of the absolute difference between the ASCII values of adjacent characters.
+# Return the score of s.
+
+class Solution(object):
+    def scoreOfString(self, s):
+        summ=0
+        for i in range(len(s)):
+            if i!=len(s)-1:
+                summ+=abs(ord(s[i])-ord(s[i+1]))
+        return summ
