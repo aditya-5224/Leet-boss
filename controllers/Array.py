c5bfecb924bee77be96b6a8d95730da46637a10c
aditya-5224 <yadi.tya5224@gmail.com>
2025-05-19 14:29:32 +0530

    array question 23

--- controllers/Array.py
+++ controllers/Array.py
@@ -301,4 +301,21 @@ class Solution(object):
         for i in range(len(nums)):
             nums[i]=nums[i]**2
         return sorted(nums)
+    
+
+# There is a programming language with only four operations and one variable X:
+# ++X and X++ increments the value of the variable X by 1.
+# --X and X-- decrements the value of the variable X by 1.
+# Initially, the value of X is 0.
+# Given an array of strings operations containing a list of operations, return the final value of X after performing all the operations.
+
+class Solution(object):
+    def finalValueAfterOperations(self, operations):
+        cnt=0
+        for i in operations:
+            if i in ["++X", "X++"]:
+                cnt+=1
+            else:
+                cnt-=1
+        return cnt
  
