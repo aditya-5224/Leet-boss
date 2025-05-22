cfab0d8f95681832d0772c2ccfc862a56efaf704
aditya-5224 <yadi.tya5224@gmail.com>
2025-05-22 14:25:14 +0530

    dsa question 2

--- controllers/DSA.py
+++ controllers/DSA.py
@@ -38,3 +38,18 @@ class Solution(object):
         :rtype: int
         """
         # ,,,,
+
+# Given an array of positive integers nums and a positive integer target, return the minimal length of a subarray whose sum is greater than or equal to target. If there is no such subarray, return 0 instead.
+
+class Solution(object):
+    def minSubArrayLen(self, target, nums):
+        left=0
+        summ=0
+        min_len=float("inf")
+        for right in range(len(nums)):
+            summ+=nums[right]
+            while summ>=target:
+                min_len=min(min_len,right-left+1)
+                summ-=nums[left]
+                left+=1
+        return min_len if min_len!=float("inf") else 0
