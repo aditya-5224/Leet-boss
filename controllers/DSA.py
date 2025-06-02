eb7c78a58d2d5f36a49a24f6d23406d9d245abe3
aditya-5224 <yadi.tya5224@gmail.com>
2025-06-02 19:53:58 +0530

    dsa question 10

--- controllers/DSA.py
+++ controllers/DSA.py
@@ -151,3 +151,20 @@ class Solution(object):
             if summ<0:
                 summ=0
         return max_sum
+    
+
+# Implement pow(x, n), which calculates x raised to the power n (i.e., xn).
+
+class Solution(object):
+    def myPow(self, x, n):
+        # return x**n
+        ans=1
+        if n<0:
+            x=1/x
+            n=-n
+        while n>0:
+            if n%2==1:
+                ans*=x
+            x*=x
+            n//=2
+        return ans
