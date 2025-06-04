1efcec956613f5ec79291fc1c86d597c61febd0a
aditya-5224 <yadi.tya5224@gmail.com>
2025-06-04 13:42:03 +0530

    dsa question 12

--- controllers/DSA.py
+++ controllers/DSA.py
@@ -18,6 +18,10 @@
 # Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
  
 
+from collections import defaultdict
+from typing import Counter
+
+
 class Solution(object):
     def lengthOfLongestSubstring(self, s):
         char_map = {}
@@ -183,4 +187,19 @@ class Solution(object):
             if prices[i]>prchase:
                 max_profit=max(max_profit,prices[i]-prchase)
         return max_profit
+    
+# Given a 0-indexed integer array nums of size n, find the maximum difference between nums[i] and nums[j] (i.e., nums[j] - nums[i]), such that 0 <= i < j < n and nums[i] < nums[j].
+# Return the maximum difference. If no such i and j exists, return -1.Given a 0-indexed integer array nums of size n, find the maximum difference between nums[i] and nums[j] (i.e., nums[j] - nums[i]), such that 0 <= i < j < n and nums[i] < nums[j].
+# Return the maximum difference. If no such i and j exists, return -1.
  
+
+class Solution(object):
+    def maximumDifference(self, nums):
+        min_num=nums[0]
+        max_diff=0
+        for i in range(1,len(nums)):
+            min_num=min(min_num,nums[i])
+            if nums[i]>min_num:
+                max_diff=max(max_diff,nums[i]-min_num)
+ 
+        return max_diff if max_diff>0 else -1
