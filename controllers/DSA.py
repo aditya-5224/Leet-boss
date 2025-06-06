69ae240178ab59f560c2778ca93c331577559586
aditya-5224 <yadi.tya5224@gmail.com>
2025-06-06 14:33:49 +0530

    dsa question 14

--- controllers/DSA.py
+++ controllers/DSA.py
@@ -224,3 +224,22 @@ class Solution(object):
                 right-=1
 
         return maxwater
+    
+
+# You are given positive integers n and m.
+# Define two integers as follows:
+
+# 1. num1: The sum of all integers in the range [1, n] (both inclusive) that are not divisible by m.
+# 2. num2: The sum of all integers in the range [1, n] (both inclusive) that are divisible by m.
+
+# Return the integer num1 - num2.
+
+class Solution(object):
+    def differenceOfSums(self, n, m):
+        summ=0
+        for i in range(1,n+1):
+            if i%m==0:
+                summ-=i
+            else:
+                summ+=i
+        return summ
