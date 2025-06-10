70cc428ea60868ba346120621ba606f866b090df
aditya-5224 <yadi.tya5224@gmail.com>
2025-06-10 16:07:55 +0530

    array question 26

--- controllers/Array.py
+++ controllers/Array.py
@@ -349,3 +349,17 @@ class Solution(object):
             else:
                 nums[i]=1
         return sorted(nums)
+    
+
+# There are n employees in a company, numbered from 0 to n - 1. Each employee i has worked for hours[i] hours in the company.
+# The company requires each employee to work for at least target hours.
+# You are given a 0-indexed array of non-negative integers hours of length n and a non-negative integer target.
+# Return the integer denoting the number of employees who worked at least target hours.
+
+class Solution(object):
+    def numberOfEmployeesWhoMetTarget(self, hours, target):
+        cnt = 0
+        for i in hours:
+            if i >= target:
+                cnt += 1
+        return cnt
