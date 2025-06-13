347fa1727f26d6b7aa834ec0302798a6218d2546
aditya-5224 <yadi.tya5224@gmail.com>
2025-06-13 20:00:24 +0530

    dsa question 17

--- controllers/DSA.py
+++ controllers/DSA.py
@@ -288,3 +288,23 @@ class Solution(object):
             else:
                 left = mid + 1
         return left
+    
+
+# Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. If target exists, then return its index. Otherwise, return -1.
+
+# You must write an algorithm with O(log n) runtime complexity.
+
+class Solution(object):
+    def search(self, nums, target):
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = left + (right - left)//2
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return -1
+        
