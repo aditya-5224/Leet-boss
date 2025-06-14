4c35574bb667b61f2e41d64bd19b7dfaceebf87a
aditya-5224 <yadi.tya5224@gmail.com>
2025-06-14 12:35:00 +0530

    dsa question 18

--- controllers/DSA.py
+++ controllers/DSA.py
@@ -307,4 +307,33 @@ class Solution(object):
             else:
                 left = mid + 1
         return -1
+    
+
+# Given an array nums with n objects colored red, white, or blue, sort them in-place so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+# We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+# You must solve this problem without using the library's sort function.
+
+class Solution(object):
+    def sortColors(self, nums):
+        n = len(nums)
+        zero = one = two = 0
+        for i in range(n):
+            if nums[i] == 0:
+                zero += 1
+            elif nums[i] == 1:
+                one += 1
+            else:
+                two += 1
+                
+        cnt = 0
+        for i in range(zero):
+            nums[cnt] = 0
+            cnt += 1
+        for i in range(one):
+            nums[cnt] = 1
+            cnt += 1
+        for i in range(two):
+            nums[cnt] = 2
+            cnt += 1
         
