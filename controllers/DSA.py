282734c0f92960bd0770975e2293c5eb54d70f96
aditya-5224 <yadi.tya5224@gmail.com>
2025-06-16 20:15:54 +0530

    dsa question 19

--- controllers/DSA.py
+++ controllers/DSA.py
@@ -336,4 +336,31 @@ class Solution(object):
         for i in range(two):
             nums[cnt] = 2
             cnt += 1
+
+
+
+
+
+# You are given an integer mountain array arr of length n where the values increase to a peak element and then decrease.
+# Return the index of the peak element.
+
+# Your task is to solve it in O(log(n)) time complexity.
+
+
+class Solution(object):
+    def peakIndexInMountainArray(self, arr):
+        if len(arr) < 3:
+            return -1
+        left = 1
+        right = len(arr) - 2
+        while left <= right:
+            mid = left + ( right - left )//2
+            if arr[mid] > arr[mid - 1] and arr[mid] > arr[mid + 1]:
+                return mid
+            if arr[mid] < arr[mid + 1]:
+                left = mid + 1
+            else:
+                right = mid - 1
+       
+        
         
