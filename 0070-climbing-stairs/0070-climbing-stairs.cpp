4764f5e50078a30c8e6c814ad2a6b2be65304efe
Aditya Yadav <yadi.tya5224@gmail.com>
2026-09-08 20:11:22 +0530

    Time: 0 ms (100%), Space: 8.7 MB (20.49%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        vector<int> dp(n+1, 0);
+        int prev1 = 1, prev2 = 1;
+        for (int i = 2; i <= n; i++){
+            int curr = prev1+prev2;
+            prev1 = prev2;
+            prev2 = curr;
+            
+
+        }
+
+        return prev2;
+    }
+};
